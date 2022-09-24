97d5444e2f6439f6c01431a1e6e9018eecc72e9b
Prashanna313 <buskey14@gmail.com>
2022-09-24 10:52:44 +0530

    update jenkinsfile

---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+python3.7 -m pip install --upgrade pip && \
+  pip install -e '.[build]'
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-python3.8 -m pip install --upgrade pip && \
-  pip install .
